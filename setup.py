9abe6a36df18ed85a93a4808d7c9da6b0303303f
IThinkImOKAY <diogenesjunior@protonmail.com>
2021-07-02 19:15:54 -0500

    lmao

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='rdrama',
-      version='0.0.1',
+      version='0.0.2',
       author='diogenesjunior',
       author_email='diogenesjunior@protonmail.com',
       description='rdrama.net api wrapper',
