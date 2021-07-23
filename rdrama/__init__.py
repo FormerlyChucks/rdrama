5e70569585351ca1e28033e5adb8eb96c757f387
dio <61555147+IThinkImOKAY@users.noreply.github.com>
2021-07-23 18:32:54 -0500

    Update __init__.py

--- rdrama/__init__.py
+++ rdrama/__init__.py
@@ -18,7 +18,7 @@ class Drama:
         if self.status_code == 200:
             return response.json()
         if self.status_code == 204:
-            return self.status_code
+            return response#really?
         else:
             raise Exception(f"{self.status_code}/{self.response_reason} on endpoint {endpoint}")
 
@@ -27,3 +27,23 @@ class Drama:
 
     def post(self, endpoint, data=None):
         return self.request('POST', endpoint, data=data)
+    """
+    def me(self):
+        #everything fine except the badges attribute
+        for key, value in self.get('/api/v1/identity').items():
+            setattr(self, key, value)
+        return self
+    
+    def badmins(self):
+        return [mod['user']['username'] for mod in drama.get('/api/v1/mod/mods')['data']]
+    
+    def submission(self,p_id):
+        for key, value in self.get(f'/api/v1/post/{p_id}').items():
+            setattr(self, key, value)
+        return self
+    
+    def comment(self,c_id):
+        for key, value in self.get(f'/api/v1/comment/{c_id}').items():
+            setattr(self, key, value)
+        return self
+    """
